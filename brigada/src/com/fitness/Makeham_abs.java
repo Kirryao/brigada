809b3c159187fc8d4e0f035bccf54ef3e74d0cda
laptop <laptop@laptop-virtual-machine>
2021-11-29 11:14:34 +0500

    Makeham_abs fixed:constructor added

--- brigada/src/com/fitness/Makeham_abs.java
+++ brigada/src/com/fitness/Makeham_abs.java
@@ -1,7 +1,12 @@
 package com.fitness;
 
 public abstract class Makeham_abs {
-	private int A,B,C;
+	private double A,B,C;
+	Makeham_abs(double A,double B,double C){
+		this.A=A;
+		this.B=B;
+		this.C=C;
+	}
 	private double prob(int age) {
 		return 1-Math.exp(-A*age-B*(Math.pow(C, age)-1)/Math.log(C));
 	}
