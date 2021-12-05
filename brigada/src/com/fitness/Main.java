ff1b998e67a5b7885364ef60ac1d90490e81932a
kirryao <kirryao@ubuntu>
2021-12-05 21:16:19 +0500

    Добавил информацию о разработчиках

--- brigada/src/com/fitness/Main.java
+++ brigada/src/com/fitness/Main.java
@@ -18,6 +18,11 @@ private JLabel info3 = new JLabel("Введите свою массу в кг");
 private JLabel info4 = new JLabel("Введите свой пол");
 private JLabel info5 = new JLabel("Введите свою категорию");
 private JLabel info6 = new JLabel("Введите cвой возраст");
+private JLabel info7 = new JLabel("Выполнил:");
+private JLabel info8 = new JLabel("ст. гр. ИВТ-323");
+private JLabel info9 = new JLabel("Кислицын К.К.");
+private JLabel info10 = new JLabel("Чепурин М.А.");
+private JLabel info11 = new JLabel("Холодилина В.В.");
 protected JTextArea output= new JTextArea();
 protected JTextField input1 = new JTextField("", 20);
 protected JTextField input2 = new JTextField("", 20);
@@ -27,13 +32,13 @@ protected JTextField input3 = new JTextField("", 20);
 public Main() {
 output.setEditable(false);
 main_GUI.setTitle ("Фитнес-калькулятор");
-main_GUI.setBounds(0,0,461,666);
+main_GUI.setBounds(0,0,461,620);
 main_GUI.setResizable(false);
 
 main_panel.setLayout(null);
 main_GUI.add(main_panel); 
 
-button_calculate.setBounds(80,310,300,50);
+button_calculate.setBounds(80,280,300,40);
 button_calculate.addActionListener(listenersClass.listenerCalculate);
 main_panel.add(button_calculate);
 
@@ -67,6 +72,17 @@ main_panel.add(info3);
 info6.setBounds(20,160,200,30);
 main_panel.add(info6);
 
+info7.setBounds(350,450,200,20);
+main_panel.add(info7);
+info8.setBounds(320,470,200,20);
+main_panel.add(info8);
+info9.setBounds(323,490,200,20);
+main_panel.add(info9);
+info10.setBounds(332,510,200,20);
+main_panel.add(info10);
+info11.setBounds(307,530,200,20);
+main_panel.add(info11);
+
 info4.setBounds(20,200,200,30);
 main_panel.add(info4);
 
@@ -82,7 +98,7 @@ main_panel.add(input2);
 input3.setBounds(200,160,241,30);
 main_panel.add(input3);
 
-output.setBounds(80,370,300,200);
+output.setBounds(80,330,300,80);
 main_panel.add(output);
 
 main_GUI.setVisible(true);
